01eb540cc02248c2075041f29662037e0ca43170
Dave Yan <fdu5526@live.com>
2015-11-23 10:33:03 -0800

    starting cat state machine

--- Assets/Resources/Scripts/Cat.cs
+++ Assets/Resources/Scripts/Cat.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class Cat : MonoBehaviour {
 
+	float speed;
+	GameObject player;
+	enum State { RunToward, Latched };
+	State currentState;
+
+
+
 	// Use this for initialization
 	void Start () {
-	
+		player = GameObject.Find("Player");
+		speed = 5f;
+	}
+
+
+
+	void ChasePlayer (Vector3 d) {
+		GetComponent<Rigidbody>().velocity = d.normalized * speed;
+	}
+
+
+
+	void StateTransitions () {
+		Vector3 d = player.GetComponent<Transform>().position - GetComponent<Transform>().position;
+		switch (currentState) {
+			case State.RunToward:
+				if (d.sqrMagnitude < 0.1f) { // RunToward => Latched
+					currentState = State.Latched;
+				} else {
+					ChasePlayer(d);
+				}
+				break;
+			case State.Latched:
+				// TODO
+				break;
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		StateTransitions();
 	}
 }
